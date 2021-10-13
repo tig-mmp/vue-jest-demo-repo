0af654fdf5399925e2510bc11acdcda911562724
Tiago Pinto <tigmmp@hotmail.com>
2021-10-13 22:35:17 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import App from "./App.vue";
+import Chat from './routes/Chat/Chat.vue';
+
+const chatComponent = Vue.component('Chat',Chat);
+
+new Vue({
+
+        el:"#App",
+        render: h => h(App),
+        components:{
+
+            "Chat":chatComponent
+
+        }
+        // ChatMessageService
+        
+    },
+    //{ second argument is loosely documented at best
+      // ChatMessageService
+    //}
+
+);
