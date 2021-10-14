741095fa7c2a8a7319586f5ba3f40f3770de3668
Tiago Pinto <tigmmp@hotmail.com>
2021-10-14 21:10:02 +0100

    jest configuration to prevent babel error

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    moduleFileExtensions: ["js", "vue"],
-    transformIgnorePatterns: ["<rootDir>/node_modules/"]
-}
+    moduleFileExtensions: ['js', 'vue'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    transform: {
+      '^.+\\.vue$': 'vue-jest',
+      '^.+\\.(js|jsx)?$': 'babel-jest'
+    },
+  }
